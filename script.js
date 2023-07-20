3534017adac2a56e2cbe894801dc7532dc38ea42
Yuval Shavit <sbcyuval@gmail.com>
2023-07-20 14:39:20 +0300

    ToDoList Finished

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function handleAddTask(event) {
-      const btnId = event.target.id;
-      console.log("clicked");
       document.getElementById("add-taskDiv").removeAttribute("hidden"); // show addTaskForm
       document.getElementById("tasks-div").setAttribute("hidden", "hidden"); // hide checkboxes
       btnAddTask.style.visibility = 'hidden';
   }
 
-  let counter= 1;
+  let counterAdd= 1;
 
-  function handleAddSubTask (event) {
+  function handleAddSubTask (event) {    
     const btnId = event.target.id;
     if (btnId === "btn-add-subTask") {
         console.log("added SubTask");
-        var new_input = `<div id='div-${counter}'><input type='text' class='added-subTask' id='txt-${counter}'></div>`; //create input text
+        let new_input = `<div id='div-${counterAdd}'><input type='text' class='added-subTask' id='txt-${counterAdd}'></div>`; //create input text
         document.getElementById("new_chq").innerHTML += (new_input); // add input text
-        counter++;
+        counterAdd++;
     }
   }
 
@@ -23,82 +21,113 @@ function handleAddTask(event) {
     event.preventDefault();
 
     let mainTask = document.getElementById("text-mainTask");
-    let subTask = document.getElementById("text-subTask");
     let subTasks = document.getElementsByClassName("added-subTask");
-    
-    var subsString = [];
-    subsString.push(subTask.value);
-    var subs = Array.prototype.slice.call( subTasks );
-    subs.forEach(element => {
-        subsString.push(element.value);
-    });
+    console.log("subTasks",subTasks);
 
-    console.log(subsString);
-    var task = {mainTask:mainTask.value, subTasks:subsString}; // create task object
-    console.log(task);
+    let subs = Array.prototype.slice.call( subTasks );
+    subs = subs.map(subTask => subTask.value);
 
-    var tasks = JSON.parse(localStorage.getItem("tasksList"));
-    console.log(tasks);
-    tasks.push(task);
-    console.log(tasks);
-    allTasks = localStorage.setItem("tasksList", JSON.stringify(tasks)); // push new task into localStrange array
+    //mainTask.value==="" || 
+    if (mainTask.value==="" || isEmpty(subs)){
+        alert("fields can't be empty");
+    }
+    else 
+    {
+                console.log("subs",subs);
+                let task = {mainTask:mainTask.value, subTasks:subs}; // create task object
+                console.log("task",task);
 
-    mainTask.value = "";
-    subTask.value="";
-    subs.forEach(element => {
-        element.value="";
-        //document.getElementById('div-${counter}').setAttribute("hidden", "hidden"); // hide checkboxes
-    }); // clear input texts
+                let tasks = JSON.parse(localStorage.getItem("tasksList"));
+            
+                tasks.push(task);
+            
+                allTasks = localStorage.setItem("tasksList", JSON.stringify(tasks)); // push new task into localStrange array
 
-    btnAddTask.style.visibility = 'visible';
-    document.getElementById("add-taskDiv").setAttribute("hidden", "hidden"); // hide addTaskForm
-    document.getElementById("tasks-div").removeAttribute("hidden"); //show tasks
+                mainTask.value = "";
+                let counter=1;
+                subs.forEach(subtask => { // clear input texts
+                    let div = document.getElementById(`div-${counter}`);
+                    div.parentNode.removeChild(div);
+                    counter++;
+                }); 
 
-    printTasks();
+                btnAddTask.style.visibility = 'visible';
+                counterAdd=1;
+                document.getElementById("add-taskDiv").setAttribute("hidden", "hidden"); // hide addTaskForm
+                document.getElementById("tasks-div").removeAttribute("hidden"); //show tasks
+
+                printTasks();
+    }
   }
+  
+function isEmpty(subs) {
+    console.log("isTrue");
+    console.log("subs",subs);
+
+    for(let subTask of subs){
+        if (subTask===""){
+            return true
+        }
+        return false
+    }
+}
 
   function printTasks () {
 
-    
-    var tasks = JSON.parse(localStorage.getItem("tasksList"));
+    let countPrint=0;
 
-    tasks.forEach(element => {
-        console.log("countPrint:"+countPrint);
-        console.log("Print tasks2:"+tasks);
-        if (countPrint<tasks.length)
-        {
-            var printMain = `<h1> <input type="checkbox" class="checkboxes" id="checkbox-${idCount}"> ${tasks[countPrint].mainTask} </h1> `;
-            document.getElementById("tasks-div").innerHTML += (printMain);
+    document.getElementById("tasks-div").innerHTML = "";
+
+    let tasks = JSON.parse(localStorage.getItem("tasksList"));
+    console.log(tasks);
+    if(tasks===null) {
+        localStorage.setItem("tasksList", JSON.stringify([]))
+        return
+    }
+
+    // display each task
+    tasks.forEach(element => { 
+        if (countPrint<tasks.length) {
             console.log("countPrint:"+countPrint);
+            console.table(tasks);
+        
+            let printMain = `<h1> <input type="checkbox" class="checkboxes" id="checkbox-${idCount}"> ${tasks[countPrint].mainTask} </h1> `;
+            document.getElementById("tasks-div").innerHTML += (printMain);
+            console.log("countPrint:",countPrint);
+            // display each sub-task
             tasks[countPrint].subTasks.forEach (element => {
+                console.table(tasks[countPrint].subTasks);
                 console.log("countId:"+countPrint);
                 idCount++;
-                var printSub = `<h2><input type="checkbox" class="checkboxes" id="checkbox-${idCount}"> ${element} </h2> `;
+                let printSub = `<h4><input type="checkbox" class="small-checkboxes" id="checkbox-${idCount}"> ${element} </h4> `;
                 document.getElementById("tasks-div").innerHTML += (printSub);
-                console.log("countPrint:"+countPrint);
+                console.log("countPrint:",countPrint);
                 
-            }); // print each sub-task
-        
-            tasks.shift();
-            //var newTasks = localStorage.setItem("tasksList", JSON.stringify(tasks)); // push new task into localStrange array
+            }); 
+            countPrint++;
+            idCount++;
         }
-    }); // print each task
-    countPrint++;
-    idCount++;
+    }); 
   }
 
-  var countPrint=0;
-  var idCount=0;
+  function handleCancel (event) {
+    document.getElementById("add-taskDiv").setAttribute("hidden", "hidden"); // hide addTaskForm
+    document.getElementById("tasks-div").removeAttribute("hidden"); //show tasks
+  }
 
-  var allTasks=[];
-  allTasks = localStorage.setItem("tasksList", JSON.stringify(allTasks));
+  let idCount=0;
 
+  printTasks();
 
   const btnAddTask = document.getElementById("btn-add-task");
   const btnAddSubTask = document.getElementById("btn-add-subTask");
+  const btnCancel = document.getElementById("btn-cancel");
+
 
   btnAddTask.addEventListener("click", handleAddTask);
   btnAddSubTask.addEventListener("click", handleAddSubTask);
+  btnCancel.addEventListener("click", handleCancel);
+
   document.getElementById("add-task-form").onsubmit = onSubmitHandler;
 
 
